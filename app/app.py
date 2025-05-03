a359e4e1fbd8c34004e5575b470e479b645fb4d3
teteutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2025-05-03 04:16:30 +0000

    add file

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+def handler(event, context):
+    # 環境変数からメッセージを取得
+    message = os.environ.get('MESSAGE', 'Hello from Lambda Container!')
+    print("Received event: " + json.dumps(event, indent=2))
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': message,
+            'version_id': context.function_version, # 実行中のバージョンID
+            'aws_request_id': context.aws_request_id
+        })
+    }
